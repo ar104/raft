288c61ffb3e8e5da0c8535d0c39d2c5cd7df80ca
root <root@arch-h2.dp.intel.com>
2016-04-15 01:55:58 -0700

    Bugfix.

--- src/raft_log.c
+++ src/raft_log.c
@@ -66,7 +66,7 @@ static void __ensurecapacity(log_private_t * me)
 
 void log_load_from_checkpoint(log_t *me_, int idx)
 {
-  log_private_t* me = (log_private_t*)calloc(1, sizeof(log_private_t));
+  log_private_t* me = (log_private_t*)me_;
   me->front = me->back = idx;
 }
 
